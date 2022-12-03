385c42627ccb040f30cff81dd0afdb3b9c7b1fc5
MarcosPassos-git <marcosbrunopassos@gmail.com>
2022-12-03 20:14:54 -0300

    Criado  onChange e onSubmit

--- pages/index.jsx
+++ pages/index.jsx
@@ -6,15 +6,28 @@ export default function HomePage() {
     const handleLabelClick = () => {
       console.log("HANDLE LABEL CLICK")
     }
+
+    const handleSubmitForm = (event) => {
+      event.preventDefault()
+      console.log('HANDLE SUBMIT FORM')
+    }
+
+    let email= ''
+    const handleChangeInput = (event) => {
+      email = event.target.value
+    }
+
   return (
     <>
       <Title>Formulário de login</Title>
       <label onClick={handleLabelClick}>Voltar para a página...</label>
-      <form>
-        <Input type="email" placeholder="Digite seu email" />
-        <Input type="password" placeholder="Digite sua senha" />
-        <Button>Botão</Button>
+      <form onSubmit={handleSubmitForm}>
+        
+        <Input type="text" placeholder="Digite sua senha" onChange={handleChangeInput}/>
+        <p>O email digitado foi: {email}</p>
+        <Button type="submit">Botão</Button>
       </form>
+      <Button>Botão</Button>
       
       <label>LABEL TESTE</label>
     </>
