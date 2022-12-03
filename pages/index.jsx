23360e3be378dd1cd8a9c6ac12ec74586e237787
MarcosPassos-git <marcosbrunopassos@gmail.com>
2022-12-03 16:31:54 -0300

    Criado onClick

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,11 +1,21 @@
 import Title from '../src/components/title/title'
 import Button from '../src/components/button/button'
+import Input from '../src/components/input/input'
 
 export default function HomePage() {
+    const handleLabelClick = () => {
+      console.log("HANDLE LABEL CLICK")
+    }
   return (
     <>
-      <Title>Título teste</Title>
-      <Button>Botão</Button>
+      <Title>Formulário de login</Title>
+      <label onClick={handleLabelClick}>Voltar para a página...</label>
+      <form>
+        <Input type="email" placeholder="Digite seu email" />
+        <Input type="password" placeholder="Digite sua senha" />
+        <Button>Botão</Button>
+      </form>
+      
       <label>LABEL TESTE</label>
     </>
   )
---
+++ src/components/input/input.jsx
@@ -0,0 +1,9 @@
+import styled from "styled-components"
+
+const StyledInput = styled.input`
+    
+`
+
+export default function Input(props) {
+    return <StyledInput {...props} />
+}
